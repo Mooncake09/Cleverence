4e53f46e4a25d6dfe14cfc27f8c6762e76926531
Zakhar Vislov <tabak1805@gmail.com>
2022-09-21 20:34:38 +0300

    Task 1. Server

--- Cleverence/Server.cs
+++ Cleverence/Server.cs
@@ -1,7 +1,34 @@
 ﻿namespace Cleverence
 {
-    public class Class1
+    public static class Server
     {
+        private static int _count;
+        private static ReaderWriterLockSlim _lock = new ();
 
+        public static int GetCount()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public static void AddCount(int value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _count += value;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
     }
 }
