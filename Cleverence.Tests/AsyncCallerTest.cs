ff43cd6334f2f92cdd8e84b0ef6a5789fc609f4b
Zakhar Vislov <tabak1805@gmail.com>
2022-09-23 00:16:18 +0300

    Task 2. Add AsyncCaller and AsyncCallerTests

---
+++ Cleverence.Tests/AsyncCallerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleverence.Tests
+{
+    [TestClass]
+    public class AsyncCallerTest
+    {
+        [TestMethod]
+        [DataRow(2000)]
+        [DataRow(5000)]
+        [DataRow(7000)]
+        public void Invoke_true(int miliseconds)
+        {
+            var eventHandler = new EventHandler((sender, args) =>
+            {
+                Console.WriteLine("Doing some work 1000 miliseconds");
+                Thread.Sleep(1000);
+            });
+
+            var asyncCaller = new AsyncCaller(eventHandler);
+            var isCompletedOnTime = asyncCaller.Invoke(miliseconds, null, EventArgs.Empty);
+
+            Assert.IsTrue(isCompletedOnTime);
+        }
+
+        [TestMethod]
+        [DataRow(700)]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(999)]
+        [DataRow(200)]
+        public void Invoke_false(int miliseconds)
+        {
+            var eventHandler = new EventHandler((sender, args) =>
+            {
+                Console.WriteLine("Doing some work 1000 miliseconds");
+                Thread.Sleep(1000);
+            });
+
+            var asyncCaller = new AsyncCaller(eventHandler);
+            var isCompletedOnTime = asyncCaller.Invoke(miliseconds, null, EventArgs.Empty);
+
+            Assert.IsFalse(isCompletedOnTime);
+        }
+    }
+}
---
+++ Cleverence/AsyncCaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleverence
+{
+    public class AsyncCaller
+    {
+        private EventHandler _eventHandler;
+
+        public AsyncCaller(EventHandler eventHandler)
+        {
+            _eventHandler = eventHandler;
+        }
+
+        public bool Invoke(int miliseconds, object? sender, EventArgs e)
+        {
+            if (miliseconds >= 0)
+            {
+                var task = Task.Run(() => _eventHandler.Invoke(sender, e));
+
+                return task.Wait(miliseconds);
+            }
+
+            throw new ArgumentException("Time miliseconds cannot be less than 0");
+        }
+    }
+}
